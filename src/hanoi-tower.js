691c13ed4d6b5be2c064b29f3855efe467689d86
Damascan-Alex <doxvirtual@gmail.com>
2020-11-01 18:17:52 +0700

    task: repeater, hanoi, vigenere

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,12 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error')
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+	turns = 2 ** disksNumber - 1
+	seconds = Math.floor((turns / turnsSpeed) * 3600)
+
+	let tower = {
+		turns: +`${this.turns}`,
+		seconds: +`${this.seconds}`,
+	}
+	return tower
+}
